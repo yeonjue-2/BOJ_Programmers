e822684ccc561f334f67d84a98dd88fc93872459
Yeonju Park <101540771+yeonjue-2@users.noreply.github.com>
2022-08-05 17:50:27 +0900

    1011번 우주선 1광년

---
+++ 1000번 이상/1011.py
@@ -0,0 +1,21 @@
+
+
+N = int(input())
+
+for i in range(N):
+    a, b = map(int, input().split())
+    distance = b - a				        # 11
+    n = 0
+    
+    while True:
+        if distance <= n * (n+1):	  # 11 < 3*4, n = 3 
+            break
+        n += 1												
+        													
+    if distance <= n ** 2:								 
+        print(n * 2 - 1)									
+        													
+    else:						    	          # 11 > 9 = 3 ** 2	 
+        print(n * 2)
+        
+        

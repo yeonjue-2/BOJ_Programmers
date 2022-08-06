aa23be365f7b6d25f06f84b69cfaa462ff8c5c5a
Yeonju Park <101540771+yeonjue-2@users.noreply.github.com>
2022-08-07 01:24:42 +0900

    11653번 소인수분해

--- 10000번 이상/11653.py
+++ 10000번 이상/11653.py
@@ -1,10 +1,15 @@
+import sys
 
-N = int(input())
+N = int(sys.stdin.readline())
 m = 2
 
 while N != 1:
     if N % m == 0:
         print(m)
         N //= m
-        continue
-    m += 1
+    else:
+        m += 1
+        
+# Q.정수 N이 주어졌을 때, 소인수분해하는 프로그램을 작성하시오.        
+# ->  처음엔 소수를 받아놓고 하나씩 꺼내려고 했음(오륲)        
+

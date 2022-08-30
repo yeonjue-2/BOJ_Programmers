707c1317ffb7f0ebc0f89b5983580a4ced2dcaef
박연주 <ilov-@naver.com>
2022-08-30 19:48:23 +0900

    2798번 블랙잭(BP)

---
+++ 1000번 이상/2798.py
@@ -0,0 +1,13 @@
+N, M = map(int, input().split())
+card = list(map(int, input().split()))
+result = 0
+
+for i in range(N):
+	for j in range(i+1, N):
+        for k in range(j+1, N):
+        	if card[i] + card[j] + card[k] > M:
+            	continue   # M보다 큰 값은 제외
+            else:
+            	result = max(result, card[i] + card[j] + card[k])
+
+print(result)
